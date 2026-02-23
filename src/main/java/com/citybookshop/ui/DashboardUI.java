a91c5c7981b6c5fd1cbb709b8705a569e4966c3b
Kanila Akarsha <156494340+KanilaAkarsha@users.noreply.github.com>
2026-02-23 23:26:39 +0530

    up

--- src/main/java/com/citybookshop/ui/DashboardUI.java
+++ src/main/java/com/citybookshop/ui/DashboardUI.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -27,8 +28,23 @@ public class DashboardUI extends Application {
         sidebar.setAlignment(Pos.CENTER);
         sidebar.setStyle("-fx-padding: 20; -fx-background-color: #1e3c72;");
 
+        Label title = new Label("📚 CityBookshop");
+        title.setStyle("-fx-font-size: 24px; -fx-text-fill: white; -fx-font-weight: bold;");
+
+        VBox titleBox = new VBox(20, title);
+        titleBox.setAlignment(Pos.CENTER);
+        titleBox.setStyle("-fx-padding: 20; -fx-background-color: #1e3c72;");
+
+        Label label = new Label("Welcome to CityBookshop Manager Dashboard");
+        label.setStyle("-fx-font-size: 25px; -fx-text-fill: white; -fx-font-weight: bold; -fx-alignment: center; -fx-wrap-text: true;-fx-text-alignment: center; -fx-padding: 20px;");
+
+        VBox welcome = new VBox(20, label);
+        welcome.setAlignment(Pos.CENTER);
+
         BorderPane root = new BorderPane();
         root.setLeft(sidebar);
+        root.setTop(titleBox);
+        root.setBottom(welcome);
         root.setCenter(content);
 
         addBook.setOnAction(e -> AddBookUI.show(content));
--- src/main/java/com/citybookshop/ui/UserUI.java
+++ src/main/java/com/citybookshop/ui/UserUI.java
@@ -4,8 +4,8 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -14,17 +14,30 @@ public class UserUI extends Application {
     @Override
     public void start(Stage stage) {
 
-        StackPane content = new StackPane();
-
         Button viewBooks = new Button("📖 View Books");
 
         VBox sidebar = new VBox(20, viewBooks);
         sidebar.setAlignment(Pos.CENTER);
         sidebar.setStyle("-fx-padding: 20; -fx-background-color: #1e3c72;");
 
+        Label title = new Label("📚 CityBookshop");
+        title.setStyle("-fx-font-size: 24px; -fx-text-fill: white;");
+
+        VBox titleBox = new VBox(20, title);
+        titleBox.setAlignment(Pos.CENTER);
+        titleBox.setStyle("-fx-padding: 20; -fx-background-color: #1e3c72;");
+
+        Label label = new Label("Welcome to CityBookshop Cashier Dashboard");
+        label.setStyle("-fx-font-size: 70px; -fx-text-fill: white; -fx-font-weight: bold; -fx-alignment: center; -fx-wrap-text: true;-fx-text-alignment: center; -fx-padding: 20px; -fx-width: 500px;");
+
+        VBox welcome = new VBox(20, label);
+        welcome.setAlignment(Pos.CENTER);
+
+
         BorderPane root = new BorderPane();
         root.setLeft(sidebar);
-        root.setCenter(content);
+        root.setTop(titleBox);
+        root.setCenter(welcome);
 
         viewBooks.setOnAction(e -> ViewBooksUI.show(new Stage()));
 
